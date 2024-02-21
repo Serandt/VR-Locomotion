c502c4a0c54f528babbd2e0fc31d037d9b8d5803
Serandt <36221849+Serandt@users.noreply.github.com>
2024-02-21 14:35:08 +0100

    clean up: deleted unnecessary comments

--- Assets/Scripts/LocomotionTechnique.cs
+++ Assets/Scripts/LocomotionTechnique.cs
@@ -75,8 +75,6 @@ public class LocomotionTechnique : MonoBehaviour
     {
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         // Please implement your LOCOMOTION TECHNIQUE in this script :D.
-        leftTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, leftController); 
-        rightTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, rightController);
 
         //Set start position for hmd and broom
         if (!PositionSetted && hmd.transform.localPosition.y > 0.5f)
@@ -116,7 +114,6 @@ public class LocomotionTechnique : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-
         // These are for the game mechanism.
         if (other.CompareTag("banner"))
         {
@@ -125,16 +122,12 @@ public class LocomotionTechnique : MonoBehaviour
         }
         else if (other.CompareTag("objectInteractionTask"))
         {
-            //selectionTaskMeasure.isTaskStart = true;
-            //selectionTaskMeasure.scoreText.text = "";
             selectionTaskMeasure.partSumErr = 0f;
             selectionTaskMeasure.partSumTime = 0f;
-            // rotation: facing the user's entering direction
             float tempValueY = other.transform.position.y > 0 ? 12 : 0;
             Vector3 tmpTarget = new Vector3(hmd.transform.position.x, tempValueY, hmd.transform.position.z);
             selectionTaskMeasure.taskUI.transform.LookAt(tmpTarget);
             selectionTaskMeasure.taskUI.transform.Rotate(new Vector3(0, 180f, 0));
-            //selectionTaskMeasure.taskStartPanel.SetActive(true);
         }
         else if (other.CompareTag("coin"))
         {
@@ -147,7 +140,6 @@ public class LocomotionTechnique : MonoBehaviour
         {
             grounded = true;
         }
-        // These are for the game mechanism.
     }
 
     private void OnTriggerExit(Collider other)
